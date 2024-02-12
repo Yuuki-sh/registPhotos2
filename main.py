d62938886419a61548e9a9945b65adde2dcc3d84
Yuuki-sh <156796005+Yuuki-sh@users.noreply.github.com>
2024-02-12 19:42:16 +0900

    main.py

--- main.py
+++ main.py
@@ -104,11 +104,15 @@ FORMAT_IMG = '<img src="{src}" />'
 #folium.Marker(location=[36.65139, 138.18111], popup=folium.Popup(iframe1)).add_to(m)  #max_width=300
 
 for a, row in df.iterrows():
-    img = 'https://skima-shinshu.com/wp-content/uploads/2019/08/P8051140-768x512.jpg'
-    html = FORMAT_IMG.format(src=img)
-    iframe = branca.element.IFrame(html=html, width=325, height=325)
-    folium.Marker(location=[row["lon"], row["lat"]], popup=folium.Popup(iframe)).add_to(m)  #max_width=300
+    #img = 'https://skima-shinshu.com/wp-content/uploads/2019/08/P8051140-768x512.jpg'
+    #html = FORMAT_IMG.format(src=img)
+    #iframe = branca.element.IFrame(html=html, width=325, height=325)
+    #folium.Marker(location=[row["lon"], row["lat"]], popup=folium.Popup(iframe)).add_to(m)  #max_width=300
 
+    pop=f"No:{row['No']}, {row['loc']}"
+    folium.Marker(location=[row["lon"], row["lat"]], 
+                  popup=folium.Popup(pop, max_width=300)
+                 ).add_to(m)  #max_width=300
 
 st_data = st_folium(m, width=700, height=800)
 
